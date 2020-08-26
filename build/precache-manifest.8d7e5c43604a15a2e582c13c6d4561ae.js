f20c262ea6537fbc8079f879987fca944d8bfc3a
Rachneet Singh Sachdeva <rachneetsachdeva@yahoo.com>
2020-08-26 11:46:05 +0200

    added build files

---
+++ build/precache-manifest.8d7e5c43604a15a2e582c13c6d4561ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad16360fbdb4522456f552e8efaf8d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b558dbe9cb1f4818bc5",
+    "url": "/static/css/2.dfbc895d.chunk.css"
+  },
+  {
+    "revision": "fcb3a9c6ebd19d08b6ee",
+    "url": "/static/css/main.6dec42e2.chunk.css"
+  },
+  {
+    "revision": "8b558dbe9cb1f4818bc5",
+    "url": "/static/js/2.0ead48c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0ead48c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb3a9c6ebd19d08b6ee",
+    "url": "/static/js/main.9ae039b1.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  },
+  {
+    "revision": "45ce0a3c706546427a183321fbe88738",
+    "url": "/static/media/gender.45ce0a3c.jpg"
+  }
+]);
