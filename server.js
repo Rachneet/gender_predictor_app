cd1c374a31565f0f51b3063c0e0c35343875c4b7
Rachneet Singh Sachdeva <rachneetsachdeva@yahoo.com>
2020-08-27 11:30:51 +0200

    update server

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
   app.listen(port);
 }
